17a4524a5492dbaa399b890f5ac9ced203aeee82
mtguerson <mtguerson@gmail.com>
2024-07-11 02:08:14 -0400

    add TripDetails page ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,25 @@
-import { MapPin, Calendar, ArrowRight } from 'lucide-react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import { CreateTripPage } from "./pages/CreateTrip";
+import { TripDetailsPage } from "./pages/TripDetails";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CreateTripPage />,
+  },
+  {
+    path: "/trips/:tripId",
+    element: <TripDetailsPage />,
+  },
+]);
 
 export function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
-      <div className="max-w-3xl w-full px-6 text-center space-y-10">
-        <div className="flex flex-col items-center gap-3">
-          <img src="/logo.svg" alt="plann.er" />
-          <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua viagem!</p>
-        </div>
-        <div>
-
-          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-            <div className="flex items-center gap-2 flex-1">
-              <MapPin className="size-5 text-zinc-400" />
-              <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
-            </div>
-
-            <div className="flex items-center gap-2">
-              <Calendar className="size-5 text-zinc-400" />
-              <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
-            </div>
-
-            <div className="w-px h-6 bg-zinc-800">
-
-            </div>
-
-            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-              Continuar
-              <ArrowRight className="size-5" />
-            </button>
-          </div>
-          
-        </div>
-        <p className="text-sm text-zinc-500">
-          Ao planejar sua viagem pela plann.er você automaticamente concorda <br /> com
-          nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade</a>.
-        </p>
-      </div>
-    </div>
+    <RouterProvider router={router} />
   );
 }
